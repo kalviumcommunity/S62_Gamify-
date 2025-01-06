6200cea3db622f8c46963bac69cba904558e8785
amityohan <amit.yohan@kalvium.community>
2025-01-06 16:55:49 +0530

    made changes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express=require('express');
 const app=express();
 
-
 // setting up an endpoint
 
 app.get('/',(req,res)=>{
@@ -14,3 +13,4 @@ const PORT=8080;
 app.listen(PORT,()=>{
     console.log(`Virtual Assistant API is running on http://localhost:${PORT}`)
 })
+
